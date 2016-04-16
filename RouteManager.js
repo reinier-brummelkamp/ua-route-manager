127cb09f052a6f3c0526e3a93d2f297185418130
Reinier Brummelkamp <reinier@urbanautomation.co.za>
2016-04-16 20:56:04 +0200

    Removed bounded functions

--- RouteManager.js
+++ RouteManager.js
@@ -10,131 +10,78 @@ var RouteManager = (function () {
         var _appContainer = appContainer;
         var _elementFactory = elementFactory;
 
-        var _loadAppHeaderBoundFx;
-        var _loadAppLeftFrameBoundFx;
-        var _loadAppCenterFrameBoundFx;
-        var _loadAppRightFrameBoundFx;
-        var _loadAppFooterBoundFx;
-
         // Private methods
-        function _loadAppHeader(elementName) {
-
-            return new Promise(function (resolve, reject) {
-                _elementFactory.createElement(elementName).then(function (element) {
-                    _appContainer.setAppHeaderContents(element);
-
-                    resolve(element);
-                }, function (error) {
-                    reject(error);
-                });
-            });
-        }
-
-        function _loadAppLeftFrame(elementName) {
-
-            return new Promise(function (resolve, reject) {
-                _elementFactory.createElement(elementName).then(function (element) {
-                    _appContainer.setAppLeftFrameContents(element);
-
-                    resolve(element);
-                }, function (error) {
-                    reject(error);
-                });
-            });
-        }
-
-        function _loadAppCenterFrame(elementName) {
-
-            return new Promise(function (resolve, reject) {
-                _elementFactory.createElement(elementName).then(function (element) {
-                    _appContainer.setAppCenterFrameContents(element);
-
-                    resolve(element);
-                }, function (error) {
-                    reject(error);
-                });
-            });
-        }
-
-        function _loadAppRightFrame(elementName) {
-
-            return new Promise(function (resolve, reject) {
-                _elementFactory.createElement(elementName).then(function (element) {
-                    _appContainer.setAppRightFrameContents(element);
-
-                    resolve(element);
-                }, function (error) {
-                    reject(error);
-                });
-            });
-        }
-
-        function _loadAppFooter(elementName) {
-
-            return new Promise(function (resolve, reject) {
-                _elementFactory.createElement(elementName).then(function (element) {
-                    _appContainer.setAppFooterContents(element);
-
-                    resolve(element);
-                }, function (error) {
-                    reject(error);
-                });
-            });
-        }
-
 
         return {
             // Public variables
             pageJs: page,
 
             // Public methods 
-
             loadAppHeader: function (elementName) {
-                if (_loadAppHeaderBoundFx)
-                    return _loadAppHeaderBoundFx(elementName);
-                else {
-                    _loadAppHeaderBoundFx = _loadAppHeader.bind(app);
-                    return _loadAppHeaderBoundFx(elementName);
-                }
+
+                return new Promise(function (resolve, reject) {
+                    _elementFactory.createElement(elementName).then(function (element) {
+                        _appContainer.setAppHeaderContents(element);
+
+                        resolve(element);
+                    }, function (error) {
+                        reject(error);
+                    });
+                });
             },
 
             loadAppLeftFrame: function (elementName) {
-                if (_loadAppLeftFrameBoundFx)
-                    return _loadAppLeftFrameBoundFx(elementName);
-                else {
-                    _loadAppLeftFrameBoundFx = _loadAppLeftFrame.bind(app);
-                    return _loadAppLeftFrameBoundFx(elementName);
-                }
+
+                return new Promise(function (resolve, reject) {
+                    _elementFactory.createElement(elementName).then(function (element) {
+                        _appContainer.setAppLeftFrameContents(element);
+
+                        resolve(element);
+                    }, function (error) {
+                        reject(error);
+                    });
+                });
             },
 
             loadAppCenterFrame: function (elementName) {
 
-                if (_loadAppCenterFrameBoundFx)
-                    return _loadAppCenterFrameBoundFx(elementName);
-                else {
-                    _loadAppCenterFrameBoundFx = _loadAppCenterFrame.bind(app);
-                    return _loadAppCenterFrameBoundFx(elementName);
-                }
+                return new Promise(function (resolve, reject) {
+                    _elementFactory.createElement(elementName).then(function (element) {
+                        _appContainer.setAppCenterFrameContents(element);
+
+                        resolve(element);
+                    }, function (error) {
+                        reject(error);
+                    });
+                });
             },
 
             loadAppRightFrame: function (elementName) {
-                if (_loadAppRightFrameBoundFx)
-                    return _loadAppRightFrameBoundFx(elementName);
-                else {
-                    _loadAppRightFrameBoundFx = _loadAppRightFrame.bind(app);
-                    return _loadAppRightFrameBoundFx(elementName);
-                }
+
+                return new Promise(function (resolve, reject) {
+                    _elementFactory.createElement(elementName).then(function (element) {
+                        _appContainer.setAppRightFrameContents(element);
+
+                        resolve(element);
+                    }, function (error) {
+                        reject(error);
+                    });
+                });
             },
 
             loadAppFooter: function (elementName) {
-                if (_loadAppFooterBoundFx)
-                    return _loadAppFooterBoundFx(elementName);
-                else {
-                    _loadAppFooterBoundFx = _loadAppFooter.bind(app);
-                    return _loadAppFooterBoundFx(elementName);
-                }
-            },
 
+                return new Promise(function (resolve, reject) {
+                    _elementFactory.createElement(elementName).then(function (element) {
+                        _appContainer.setAppFooterContents(element);
+
+                        resolve(element);
+                    }, function (error) {
+                        reject(error);
+                    });
+                });
+            },
+            
             registerRoute: function (url, callback) {
                 page(url, callback);
             },
